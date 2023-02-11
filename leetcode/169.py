aaee39ea9dc76c0a716ddedc518ce8f83fc2e609
Nadiveedi Shravan Reddy <nshravanreddy6@gmail.com>
2023-02-11 18:47:07 +0530

    11-02-2022

---
+++ leetcode/169.py
@@ -0,0 +1,11 @@
+nums = [3,2,3]
+a={}
+for i in range(len(nums)):
+    if i not in a:
+        a[nums[i]]=1
+    else:
+        a[nums[i]]+=1
+print(a)
+for k,v in a.items():
+    if v>len(nums)//2:
+        print(k)
