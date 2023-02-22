50986e2f405df3fc7d52991f9b617b9585f48a38
Nadiveedi Shravan Reddy <nshravanreddy6@gmail.com>
2023-02-22 20:08:11 +0530

    22-02-2023

---
+++ functions/add&mul.py
@@ -0,0 +1,21 @@
+#Hello, now I am going to write about the functions
+#What is Function?
+#Think about in programming. If you are familiar check out my explanation too
+#Function is a set of codes that are combined together to run. 
+#The use of functions is code resusability and readability.
+ 
+#Write a program code to find addition and multiplication of two number
+
+#creating the addition function
+def add(n1,n2):
+    sum=n1+n2
+    return sum
+def mult(n1,n2):
+    multi=n1*n2
+    return multi
+
+result=add(10,20)
+print(result)
+result=mult(10,20)
+print(result)
+
